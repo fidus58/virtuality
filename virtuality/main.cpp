2f1bc7f1c48d2743770843b1ffd7368363a091fe
fidus58 <ahrens@informatik.hu-berlin.de>
2021-05-31 19:04:27 +0200

    Initial Commit

---
+++ virtuality/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  virtuality
+//
+//  Created by Klaus Ahrens on 31.05.21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
