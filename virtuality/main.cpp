41a12f0a28eed9d5ed30d333853d9f1bd69ce949
fidus58 <ahrens@informatik.hu-berlin.de>
2021-06-01 13:36:16 +0200

    final

--- virtuality/main.cpp
+++ virtuality/main.cpp
@@ -7,8 +7,47 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+class A {
+public:
+    virtual void foo(){std::cout<<"A::foo()\n";}
+};
+
+class B : public A {
+public:
+    void foo() override {std::cout<<"B::foo()\n";}
+};
+
+class C : public B {
+public:
+    virtual void foo(int=0) {std::cout<<"C::foo(int)\n";}
+};
+
+class D : public B {
+    
+};
+
+int main() {
+    
+    A a; B b; C c; D d;
+    
+    a.foo();
+    b.foo();
+    c.foo();
+    c.foo(2);
+    d.foo();
+
+    A* pa = nullptr;
+    
+    pa = &a;
+    pa->foo();
+    pa = &b;
+    pa->foo();
+    pa = &c;
+    pa->foo();
+
+
+    pa = &d;
+    pa->foo();
+    
     return 0;
 }
